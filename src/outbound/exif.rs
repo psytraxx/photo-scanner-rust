5412b229812cab5ce78899bd3dd7042cf5194ada
eric <psytraxx@gmail.com>
2024-10-09 20:57:13 +0200

    XP_COMMENT tag writing fixed

--- src/outbound/exif.rs
+++ src/outbound/exif.rs
@@ -1,27 +1,62 @@
-use std::path::Path;
+use std::{char::decode_utf16, path::Path};
 
 use crate::domain::ports::FileMeta;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use little_exif::{exif_tag::ExifTag, metadata::Metadata};
+use little_exif::{endian::Endian, exif_tag::ExifTag, metadata::Metadata};
+use tracing::{debug, info};
+
+const XP_COMMENT: u16 = 0x9C9C;
 
 #[derive(Debug, Clone)]
 pub struct EXIF {}
+
+/// Converts a byte slice in UCS-2 little-endian format to a String.
+fn ucs2_little_endian_to_string(bytes: &[u8]) -> Result<String> {
+    if bytes.len() % 2 != 0 {
+        return Err(anyhow!("Invalid byte array length for UCS-2"));
+    }
+
+    let u16_data: Vec<u16> = bytes
+        .chunks(2)
+        .map(|chunk| u16::from_le_bytes([chunk[0], chunk[1]]))
+        .collect();
+
+    decode_utf16(u16_data)
+        .map(|r| r.map_err(|e| format!("Invalid UTF-16 code unit: {}", e)))
+        .collect::<Result<String, _>>()
+        .map_err(|e| anyhow!(e.to_string()))
+}
+
+/// Converts a string to a byte vector in UCS-2 little-endian format.
+fn string_to_ucs2_little_endian(input: &str) -> Vec<u8> {
+    input
+        .encode_utf16()
+        .flat_map(|unit| unit.to_le_bytes())
+        .collect()
+}
 
 #[async_trait]
 impl FileMeta for EXIF {
     async fn write(&self, text: &str, path: &Path) -> Result<()> {
         let mut metadata = Metadata::new_from_path(path)?;
-        let ucs2_bytes: Vec<u8> = text
-            .encode_utf16()
-            .flat_map(|c| c.to_le_bytes()) // Convert each u16 to a 2-byte little-endian representation
-            .collect();
-        //https://exiftool.org/TagNames/EXIF.html
-        metadata.set_tag(ExifTag::UnknownUNDEF(
-            ucs2_bytes,
-            0x9c9c,
+
+        match metadata.get_tag_by_hex(XP_COMMENT) {
+            Some(tag) => {
+                let comment = ucs2_little_endian_to_string(&tag.value_as_u8_vec(&Endian::Little));
+                info!("Tag already exists: {:?}", comment);
+            }
+            None => {
+                debug!("Tag does not exist");
+            }
+        }
+
+        metadata.set_tag(ExifTag::UnknownINT8U(
+            string_to_ucs2_little_endian(text),
+            XP_COMMENT,
             little_exif::exif_tag::ExifTagGroup::IFD0,
         ));
+
         metadata.write_to_file(path)?;
         Ok(())
     }
